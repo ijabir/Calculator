133c52806cfbd2898319bb04fd3d82076ebddf38
ijabir <jabirikamal@gmail.com>
2015-11-04 03:31:13 +0600

    upda

--- app/src/main/java/com/example/administrator/calculator/MainActivity.java
+++ app/src/main/java/com/example/administrator/calculator/MainActivity.java
@@ -13,35 +13,85 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        s = (TextView)findViewById(R.id.screen);
     }
 
     int a, b, r;
+    TextView s;
 
     public void equal(View view)
     {
 
     }
 
-    public void b7(View view
-    {
-
-    }
-
     public void bD(View view)
     {
-        r=a/b;
+        s.append("/");
     }
 
     public void bM(View view)
     {
-        r=a/b;
+        s.append("*");
     }
 
     public void bS(View view)
     {
-        r=a/b
+        s.append("-");
     }
 
-    public void bA(View view) {
+    public void bA(View view)
+    {
+        s.append("+");
     }
+
+    public void b1(View view)
+    {
+        s.append("1");
+    }
+
+    public void b2(View view)
+    {
+        s.append("2");
+    }
+
+    public void b3(View view)
+    {
+        s.append("3");
+    }
+
+    public void b4(View view)
+    {
+        s.append("4");
+    }
+
+    public void b5(View view)
+    {
+        s.append("5");
+    }
+
+    public void b6(View view)
+    {
+        s.append("6");
+    }
+
+    public void b7(View view)
+    {
+        s.append("7");
+    }
+
+    public void b8(View view)
+    {
+        s.append("8");
+    }
+
+    public void b9(View view)
+    {
+        s.append("9");
+    }
+
+    public void b0(View view)
+    {
+        s.append("0");
+    }
+
 }
